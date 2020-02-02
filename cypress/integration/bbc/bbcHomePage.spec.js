91cb0209ebe43bfa1654961df743694c3327754b
meriem26 <mmlys89@gmail.com>
2020-02-02 15:46:06 +0000

    Test for the different elements in the home page

--- cypress/integration/bbc/bbcHomePage.spec.js
+++ cypress/integration/bbc/bbcHomePage.spec.js
@@ -15,4 +15,17 @@ describe("BBC Home page", function() {
       'none, url("https://nav.files.bbci.co.uk/orbit/1b2e292884201dd13064a9204e177864/img/blq-orbit-blocks_grey.svg")'
     );
   });
+  it("It should load main top stories", () => {
+    cy.get(".uk-hero-promos-container > .hp-module__container").should(
+      "be.visible"
+    );
+  });
+  it("It should contain first main story", () => {
+    cy.get(".hp-bp-m-one-half").should("be.visible");
+  });
+  it("It should contain second main story", () => {
+    cy.get(
+      '.top-story__wrapper.top-story--small-image'
+    ).should("be.visible");
+  });
 });
