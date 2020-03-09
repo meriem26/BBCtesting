803ab53ce97b5d50c7dd09280070b181b1cb02f6
meriem26 <mmlys89@gmail.com>
2020-03-09 13:08:52 +0000

    Add a new test to check the URL

--- cypress/integration/bbc/bbcHomePage.spec.js
+++ cypress/integration/bbc/bbcHomePage.spec.js
@@ -3,35 +3,38 @@ describe("BBC Home page", function() {
   beforeEach(() => {
     cy.visit("/");
   });
-  it("It should contain a title", () => {
+  it("Should return the Base URL",()=>{
+     cy.url().should('equal',"https://www.bbc.co.uk/");
+  })
+  it("Should contain a title", () => {
     HomePage.getBanner().should("contain.text", "Welcome to the BBC");
   });
-  it("It should contain a nav-bar", () => {
+  it("Should contain a nav-bar", () => {
     HomePage.navBar().should("be.visible");
   });
-  it("It should contain logo image of BBC ", () => {
+  it("Should contain logo image of BBC ", () => {
     HomePage.logo().should(
       "have.css",
       "background-image",
       'none, url("https://nav.files.bbci.co.uk/orbit/1b2e292884201dd13064a9204e177864/img/blq-orbit-blocks_grey.svg")'
     );
   });
-  it("It should load main top stories", () => {
+  it("Should load main top stories", () => {
     TopStories.mainTopStories().should("be.visible");
   });
-  it("It should contain first main story", () => {
+  it("Should contain first main story", () => {
     TopStories.firstMainStory().should("be.visible");
   });
-  it("It should contain second main story", () => {
+  it("Should contain second main story", () => {
     TopStories.secondMainStory().should("be.visible");
   });
-  it("It should contain an image in each story", () => {
+  it("Should contain an image in each story", () => {
     TopStories.imageForEachTopStory().should("be.visible");
   });
-  it("It should contain section", () => {
+  it("Should contain section", () => {
     cy.get("section:nth-child(1)").should("be.visible");
   });
-  it("It should contain an image for each story", () => {
+  it("Should contain an image for each story", () => {
     TopStories.imageForEachStory().each($module => {
       cy.wrap($module)
         .find(".top-story__image")
